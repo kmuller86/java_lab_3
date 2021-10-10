004a280e832cdbea0330c4874baaf4a8616e426d
krzys <krzysztofmuller1@gmail.com>
2021-10-10 10:14:02 +0200

    przyklad 5

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        /* ćw 4 klasy */
+        /* Przykład 5 Dziedziczenie */
 
-        /* zad.1
-            a) Stworzyć klasę Osoba z polami prywatnymi:
-            - imie (String),
-            - nazwisko (String),
-            - indeks (Integer)
-
-            b) stworzyc konstruktory, gettery i settery
-            c) napisać metodę w klasie Osoba która zwróci informację o osobie,
-         */
-
-        /* zad.2
-            a) Stworzyć klasę Student z polami prywatnymi
-            - Osoba osoba,
-            - WydzialEnum wydzial;
-            b) stworzyć enum WydzialEnum (w nowym pliku) z kilkoma polami,
-            c) stworzyc konstruktory, gettery i settery
-            d) nadpisać metodę toString aby zwracała wszystkie elementy ładnie przedstawione :)
-         */
-
-        /* zad.3
-            a) W klasie Main stworzyć 5 różnych studentów, (można za pomocą tego samego objektu)
-            b) dodać studentów do Listy ArrayList,
-            c) przeiterować listę za pomocą pętli forEach wywołując na każdym objekcie metodę toString() (w każdej iteracji);
-         */
 
     }
 }
---
+++ src/Osoba.java
@@ -0,0 +1,18 @@
+public class Osoba {
+
+    String imie;
+    String nazwisko;
+    int pieniadze;
+
+    public Osoba() {
+        this.imie = "";
+        this.nazwisko = "";
+        this.pieniadze = 0;
+    }
+
+    public Osoba(String imie, String nazwisko, int pieniadze) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.pieniadze = pieniadze;
+    }
+}
