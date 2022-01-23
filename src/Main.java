2b9f8ae22f40eb05f3882030b7777fd10443bd66
krzys <krzysztofmuller1@gmail.com>
2022-01-24 00:08:19 +0100

    przykład 1

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,42 @@ public class Main {
         /* Użycie enumów z booleanem i metodą */
         drukujDoKonsoli(WielkoscEnum.MALY, "dlugopis");
         drukujDoKonsoli(WielkoscEnum.DUZY, "Dom");
+        System.out.println();
+
+
+        /* przykład 1b enum-y */
+        boolean[] prawdaArray = {false, false, true, false, true, true};
+        for (boolean zmienna : prawdaArray) {
+            if (zmienna) {
+                System.out.println("Czy prawda: " + PrawdaEnum.TAK);
+            } else {
+                System.out.println("Czy prawda: " + PrawdaEnum.NIE);
+            }
+        }
+        System.out.println();
+
+        /* przykład 1c enum-y */
+        char[] znaki = {'S', 'A', 'H', 'A', 'A', 'H', 'A', 'N'};
+
+        for (char znak : znaki) {
+            switch (znak) {
+                case 'S':
+                    System.out.println("Status: " + StatusEnum.USUNIETE);
+                    break;
+                case 'A':
+                    System.out.println("Status: " + StatusEnum.AKTUALNE);
+                    break;
+                case 'N':
+                    System.out.println("Status: " + StatusEnum.NIE_AKTUALNE);
+                    break;
+                case 'H':
+                    System.out.println("Status: " + StatusEnum.HISTORYCZNE);
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + znak);
+            }
+        }
+
     }
 
     static void drukujDoKonsoli(WielkoscEnum wartosc, String przedmiot) {
@@ -36,3 +72,22 @@ enum WielkoscEnum {
     }
 }
 
+enum PrawdaEnum {
+    TAK(true), NIE(false);
+
+    final boolean prawda;
+
+    PrawdaEnum(boolean czyPrawda) {
+        prawda = czyPrawda;
+    }
+}
+
+enum StatusEnum {
+    AKTUALNE('A'), NIE_AKTUALNE('N'), HISTORYCZNE('H'), USUNIETE('S');
+
+    final char status;
+
+    StatusEnum(char status) {
+        this.status = status;
+    }
+}
